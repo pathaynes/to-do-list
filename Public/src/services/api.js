a3e65d7c5699d1a7131b821e5839da9abde69135
Pat Haynes <pathaynes32@gmail.com>
2019-08-28 12:39:51 -0700

    working on email

--- Public/src/services/api.js
+++ Public/src/services/api.js
@@ -1,4 +1,13 @@
+import store from './store.js';
+
 const URL = 'api';
+
+const token = store.getToken();
+if(!token && location.pathname !== '/auth.html') {
+    const searchParams = new URLSearchParams();
+    searchParams.set('redirect', location.pathname);
+    location = `auth.html?${searchParams.toString()}`;
+}
 
 function fetchWithError(url, options) {
     return fetch(url, options)
@@ -17,7 +26,6 @@ function fetchWithError(url, options) {
 export function getTypes(options) {
     const showAll = options && options.showAll;
     const url = `${URL}/types${showAll ? '?show=all' : ''}`;
-    console.log(url);
     return fetchWithError(url);
 }
 
@@ -50,5 +58,26 @@ export function removeType(id) {
         method: 'DELETE'
     });
 }
+
+export function signUp(user) {
+    const url = `${URL}/auth/signup`;
+    return fetchWithError(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user)
+    });
+}
     
+export function signIn(credentials) {
+    const url = `${URL}/auth/signin`;
+    return fetchWithError(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(credentials)
+    });
+}
 
--- server.js
+++ server.js
@@ -5,10 +5,33 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const client = require('./lib/client');
 
 // Database Client
-client.connect();
+const client = require('./lib/client');
+
+// Auth
+const ensureAuth = require('./lib/auth/ensure-auth');
+const createAuthRoutes = require('./lib/auth/create-auth-routes');
+const authRoutes = createAuthRoutes({
+    selectUser(email) {
+        return client.query(`
+            SELECT id, email, hash, display_name as "displayName" 
+            FROM users
+            WHERE email = $1;
+        `,
+        [email]
+        ).then(result => result.rows[0]);
+    },
+    insertUser(user, hash) {
+        return client.query(`
+            INSERT into users (email, hash, display_name)
+            VALUES ($1, $2, $3)
+            RETURNING id, email, display_name as "displayName";
+        `,
+        [user.email, hash, user.displayName]
+        ).then(result => result.rows[0]);
+    }
+});
 
 // Application Setup
 const app = express();
@@ -17,6 +40,14 @@ app.use(morgan('dev')); // http logging
 app.use(cors()); // enable CORS request
 app.use(express.static('public')); // enable serving files from public
 app.use(express.json()); // enable reading incoming json data
+
+// setup authentication routes
+app.use('/api/auth', authRoutes);
+
+
+// everything that starts with "/api" below here requires an auth token!
+app.use('/api', ensureAuth);
+
 
 app.get('/api/types', (req, res) => {
     const showAll = (req.query.show && req.query.show.toLowerCase() === 'all');
